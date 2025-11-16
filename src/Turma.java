de0ca1c9da69255dc3e2eadd8057e1e4e0aefdb1
Serminho <samuelegsonmr@gmail.com>
2025-11-16 01:49:31 -0300

    Melhoria e correção para a Classe Turma

--- src/Turma.java
+++ src/Turma.java
@@ -20,27 +20,57 @@ public class Turma {
     }
 
     public boolean adicionarAluno(Aluno aluno) {
+        if (alunos.contains(aluno)) {
+            System.out.println("O aluno " + aluno.getNome() + " já está na turma.");
+            return false;
+        }
+
         if (alunos.size() < limiteAlunos) {
             alunos.add(aluno);
-            System.out.println("Aluno " + aluno.getNome() + " adicionado à turma " + codigo);
+            System.out.println("Aluno " + aluno.getNome() + " adicionado a turma " + codigo);
             return true;
         }
 
         if (aluno.isVip()) {
-            for (int i = alunos.size(); i >= 1; i--) {
+            for (int i = 0; i < alunos.size(); i++) {
                 if (!alunos.get(i).isVip()) {
                     String alunoSubstituido = alunos.get(i).getNome();
                     alunos.set(i, aluno);
-                    System.out.println("Aluno VIP " + aluno.getNome() + " adicionadoo à turma " + codigo + ", substituindo o aluno " + alunoSubstituido);
+                    System.out.println("Aluno VIP " + aluno.getNome()
+                            + " adicionado à turma " + codigo
+                            + ", substituindo o aluno " + alunoSubstituido);
                     return true;
                 }
             }
         }
-        System.out.println("Não foi possível adicionar o aluno " + aluno.getNome() + " à turma " + codigo + ": limite atingido.");
+
+        System.out.println("Não foi possível adicionar o aluno " + aluno.getNome() +
+                " à turma " + codigo + ": limite atingido.");
         return false;
+    }
+
+    public boolean removerAluno(Aluno aluno) {
+        if (alunos.remove(aluno)) {
+            System.out.println("Aluno " + aluno.getNome() + " removido da turma " + codigo);
+            return true;
+        }
+        System.out.println("Aluno " + aluno.getNome() + " não encontrado na turma " + codigo);
+        return false;
+    }
+
+    public boolean temVaga() {
+        return alunos.size() < limiteAlunos;
     }
 
     public List<Aluno> getAlunos() {
         return alunos;
+    }
+
+    public int getQuantidadeAlunos() {
+        return alunos.size();
+    }
+
+    public String getCodigo() {
+        return codigo;
     }
 }
