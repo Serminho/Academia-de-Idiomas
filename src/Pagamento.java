85f8d699a21b71c0bab2104fafbf32f946296f64
Serminho <samuelegsonmr@gmail.com>
2025-11-21 18:40:45 -0300

    Deixando a classe Pagamento mais limpa

--- src/Pagamento.java
+++ src/Pagamento.java
@@ -3,15 +3,17 @@ import java.time.LocalDate;
 public class Pagamento {
     private double valor;
     private LocalDate data;
-    private String descricao;
 
-    public Pagamento(double valor, LocalDate data, String descricao) {
+    public Pagamento(double valor, LocalDate data) {
         this.valor = valor;
         this.data = data;
-        this.descricao = descricao;
     }
 
-    public double getValor() { return valor; }
-    public LocalDate getData() { return data; }
-    public String getDescricao() { return descricao; }
+    public double getValor() {
+        return valor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
 }
