3413b499f3bdab097927388efae9066ae155c14e
Serminho <samuelegsonmr@gmail.com>
2025-11-21 20:28:56 -0300

    Correção, arquivo havia sido alterado de forma errado

--- src/Curso.java
+++ src/Curso.java
@@ -1,16 +1,11 @@
 public class Curso {
     private String nome;
-    private String nivel;
 
-    public Curso(String nome , String nivel) {
+    public Curso(String nome) {
         this.nome = nome;
-        this.nivel = nivel;
     }
-
+    
     public String getNome() {
         return nome;
-    }
-    public String getNivel() {
-        return nivel;
     }
 }
