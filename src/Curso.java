ab27c343a9c123f39fd0828d36bbfade1e0812ef
JPSantiago01 <joaop00377@gmail.com>
2025-11-22 05:50:40 -0300

    Refactor Curso class constructors and fields
    Adaptação da classe curso para o cadastro

--- src/Curso.java
+++ src/Curso.java
@@ -1,11 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curso {
     private String nome;
+    private Professor professor;
+    private String idioma;
+    private List<String> niveis;
+    private double preco;
 
-    public Curso(String nome) {
+    public Curso(String nome, Professor professor) {
         this.nome = nome;
+        this.professor = professor;
+        this.idioma = "Geral";
+        this.niveis = new ArrayList<>();
+        this.niveis.add("Básico");
+        this.preco = 1000.0;
     }
-    
+
+    public Curso(String nome, String idioma, String nivel, Professor professor, double preco) {
+        this.nome = nome;
+        this.idioma = idioma;
+        this.professor = professor;
+        this.niveis = new ArrayList<>();
+        this.niveis.add(nivel);
+        this.preco = preco;
+    }
+
+    // Getters
     public String getNome() {
         return nome;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public String getNivel() {
+        return niveis.isEmpty() ? "" : niveis.get(0);
+    }
+
+    public List<String> getNiveis() {
+        return new ArrayList<>(niveis);
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - " + idioma + " (" + getNivel() + ") - Prof. " + professor.getNome() + " - R$ " + preco;
     }
 }
